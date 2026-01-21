f26eb29721c36155562be0b09dcd7d93742f0aa7
Mel <melissapixell@gmail.com>
2026-01-21 21:49:40 +0300

    feat: switch database to PostgreSQL

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
     }
 }
 
