ab41e2720e7b91c038fe86b39896bf76190769ef
Mel <melissapixell@gmail.com>
2026-01-22 16:08:50 +0300

    Feat(profile): add  message after update

--- account/views.py
+++ account/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserRegistrationForm, \
                     UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 
 # Create your views here.
@@ -54,13 +54,16 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                    'successfully')
 
-            return redirect('edit')     # временная затычка
+        else:
+            messages.error(request, 'Error updating your profile')
 
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-        return render(request,
-                    'account/edit.html',
-                    {'user_form': user_form,
-                    'profile_form': profile_form})
+    return render(request,
+                'account/edit.html',
+                {'user_form': user_form,
+                'profile_form': profile_form})
